ca78ffaef28d0178904867bd5a6091633c89a990
Raghav Bali <raghavbali96@yahoo.com>
2021-10-14 00:41:16 +0000

    fixed bug in covers/views.py

--- covers/views.py
+++ covers/views.py
@@ -3,11 +3,11 @@ from django.shortcuts import render, get_object_or_404
 from .models import Cover
 
 def pianocovers(request):
-    covers = Cover.objects.all().order_by('cover_id')
+    covers = Cover.objects.all().order_by('id')
     return render(request, 'covers/pianocovers.html', {'covers':covers})
 
 def guitarcovers(request):
-    covers = Cover.objects.all().order_by('cover_id')
+    covers = Cover.objects.all().order_by('id')
     return render(request, 'covers/guitarcovers.html', {'covers':covers})
 
 def coverdetail(request, cover_id):
